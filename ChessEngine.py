210eb86d066983f0a894f81dc41484d54ee7f85b
Ky.ASD <p.kystaal@gmail.com>
2024-01-05 08:37:46 +1100

    Add chess notation generation, add move log

--- ChessEngine.py
+++ ChessEngine.py
@@ -21,3 +21,33 @@ class GameState():
         ]
         self.white_to_move = True
         self.move_log = []
+
+    def make_move(self, move):
+        self.board[move.start_row][move.start_column] = "--"
+        self.board[move.end_row][move.end_column] = move.piece_moved
+        self.move_log.append(move) #add to log, it can be undone or reviewed after
+        self.white_to_move = not self.white_to_move #swap players
+
+class Move():
+
+    ranks_to_rows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                     "5": 3, "6": 2, "7": 1}
+    rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
+    files_to_columns = {"a": 0, "b": 1, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
+    columns_to_files = {v: k for k, v in files_to_columns.items()}
+
+    def __init__(self, start_square, end_square, board):
+        self.start_row = start_square[0]
+        self.start_column = start_square[1]
+        self.end_row = end_square[0]
+        self.end_column = end_square[1]
+        self.piece_moved = board[self.start_row][self.start_column]
+        self.piece_captured = board[self.end_row][self.end_column]
+
+    def get_chess_notation(self):
+        #make the move notation follow chess ranks and files
+        return self.get_rank_file(self.start_row, self.start_column) + self.get_rank_file(self.end_row, self.end_column)
+
+    def get_rank_file(self, row, column):
+        return self.columns_to_files[column] + self.rows_to_ranks[row]
+    
